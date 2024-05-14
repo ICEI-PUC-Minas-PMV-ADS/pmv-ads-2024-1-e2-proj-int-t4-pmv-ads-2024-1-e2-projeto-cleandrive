02560cf7621fa2f395f4eb69c6eeaa68270b68b5
isaqueferreira <144289972+isaqueferreira@users.noreply.github.com>
2024-05-14 11:01:18 -0300

    Add files via upload

---
+++ src/CleanDrive/db_frota/inserirCarros.sql
@@ -0,0 +1,18 @@
+insert into carros
+    ( marca, modelo, autonomia_km, capacidade, malas, valor_diaria)
+VALUES 
+    ('Tesla', 'Model S', 500, 5, 2, 300.00),
+    ('Nissan', 'Leaf', 300, 5, 2, 200.00),
+    ('Chevrolet', 'Bolt', 400, 5, 3, 250.00),
+    ('BMW', 'i3', 260, 4, 2, 280.00),
+    ('Audi', 'e-tron', 350, 5, 3, 320.00),
+    ('Hyundai', 'Kona Electric', 415, 5, 2, 230.00),
+    ('Kia', 'Soul EV', 385, 5, 2, 220.00),
+    ('Jaguar', 'I-PACE', 377, 5, 3, 350.00),
+    ('Ford', 'Mustang Mach-E', 370, 5, 3, 300.00),
+    ('Volkswagen', 'ID.4', 400, 5, 2, 270.00),
+    ('Mercedes-Benz', 'EQC', 375, 5, 3, 340.00),
+    ('Porsche', 'Taycan', 320, 4, 2, 400.00),
+    ('Rivian', 'R1T', 300, 5, 3, 380.00),
+    ('Lucid', 'Air', 517, 5, 2, 450.00),
+    ('Fisker', 'Ocean', 380, 5, 2, 280.00);
